326bd320208a23ab4e17d57e97e452f0a979f7d1
Te270 <79935865+Te270@users.noreply.github.com>
2021-10-31 13:37:47 +0200

    Доделала стили

--- src/App.js
+++ src/App.js
@@ -23,7 +23,6 @@ const App = () => {
         stats={user.stats}
       />
       <Statistics title="Upload stats" stats={statisticalData} />
-      <Statistics stats={statisticalData} />
       <FriendList friends={friends} />
       <TransactionHistory items={transactions} />;
     </div>
--- src/components/Statistics/StatisticItem.js
+++ src/components/Statistics/StatisticItem.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './Statistics.module.css';
-import generatorColor from '../../color_generate';
+import generatorColor from './color_generate';
 
 console.log('colorRange: ', generatorColor());
 
